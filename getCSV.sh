c6b902b227abdb4b6e81d8a747b9e4cac1f4a573
Mark Farber <mark@yna.co.il>
2021-03-24 03:17:59 +0200

    24-03_03-17-59

--- getCSV.sh
+++ getCSV.sh
@@ -4,7 +4,7 @@
 while true
 do
 	D=$(date +"%d-%m_%H-%M-%S")
-	wget 'https://media24.bechirot.gov.il/files/expb.csv' -O'expb_$D.csv'
+	wget 'https://media24.bechirot.gov.il/files/expb.csv' -O'expb_'$D'.csv'
 	git add *
 	git commit -m $D
 	git push origin master
