5b2f254a7fe9b2f09c50e90a8f66d31e5b5a6e0d
Mark Farber <mark@yna.co.il>
2021-03-24 03:16:18 +0200

    $D

--- getCSV.sh
+++ getCSV.sh
@@ -3,10 +3,10 @@
 
 while true
 do
-	d = date +"%d-%m_%H-%M-%S"
-	wget 'https://media24.bechirot.gov.il/files/expb.csv' -O'expb_$d.csv'
+	D=$(date +"%d-%m_%H-%M-%S")
+	wget 'https://media24.bechirot.gov.il/files/expb.csv' -O'expb_$D.csv'
 	git add *
-	git commit -m '$d'
+	git commit -m '$D'
 	git push origin master
 	sleep 5m
 done
